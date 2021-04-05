f04d1d080b520de6db33d6d027bcae7265de71e7
chegrofelix <chegerofelix@gmail.com>
2021-04-05 10:05:39 +0300

    added url patterns

--- camden/urls.py
+++ camden/urls.py
@@ -1,8 +1,16 @@
-from django.urls import path
+from django.conf.urls import url
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns=[
-    path('')
+     url(r'^$',views.home,name='home'),
+    url(r'awwards/project/(\d+)',views.rate_project,name='rate-project'),
+    url(r'awwards/profile',views.view_profile,name='view-profile'),
+    url(r'^search/', views.search_project, name='search_project'),
+    url(r'^new/project$', views.new_project, name='new_project'),
+    url(r'^awwardsapi/api/profile/$', views.ProfileList.as_view(),name='api-profile'),
+    url(r'^awwardsapi/api/project/$', views.ProjectList.as_view(),name='api-project'),
+    url(r'^awwardsapi/$',views.api_page,name='api-page'),
+    url('register/',views.register, name='register'),
 ]
--- camden_awards/settings.py
+++ camden_awards/settings.py
@@ -77,8 +77,10 @@ WSGI_APPLICATION = 'camden_awards.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'camden',
+        'USER': 'moringa',
+        'PASSWORD':'ragnarok'
     }
 }
 
